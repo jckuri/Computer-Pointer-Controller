00c7b92a090504827a0045e730a7ea450a1b53e3
Juan Carlos Kuri Pinto <jckuri@gmail.com>
2020-09-04 00:43:56 -0500

    Add files via upload

---
+++ models/download_models.sh
@@ -0,0 +1,5 @@
+python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name "face-detection-adas-binary-0001"
+python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name "head-pose-estimation-adas-0001"
+python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name "landmarks-regression-retail-0009"
+python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name "gaze-estimation-adas-0002"
+
